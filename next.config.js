aeacc15ac57fc47f6265dd4c80d606d60d9bbb61
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-02-14 22:34:42 -0300

    fix: add image-pattern tmdb

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'api.ts']
+  pageExtensions: ['page.tsx', 'api.ts'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hips.hearstapps.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org'
+      }
+    ],
+  },
 }
 
 module.exports = nextConfig
