da9071cc2d4e938363b6e5e520e5fbe901d22df6
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-02-24 17:58:04 -0300

    feat: add loading bar on router change

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
+import { Router } from "next/router"
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
+import NProgress from 'nprogress'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@/styles/globals.css'
+import '@/styles/nprogress.css'
+
+Router.events.on("routeChangeStart", (url) => {
+  NProgress.start();
+})
+
+Router.events.on("routeChangeComplete", (url) => {
+  NProgress.done();
+})
+Router.events.on("routeChangeError", (url) => {
+  NProgress.done();
+})
+
+
+
+export default function App({ 
+  Component, 
+  pageProps: { session, ...pageProps } 
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
